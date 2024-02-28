e20ad517fd4245f33db4a614f9f764eabd9c1907
Thomas <thom.cherryhomes@gmail.com>
2024-02-27 18:21:36 -0600

    [coco] properly guard pause.

--- src/coco/pause.c
+++ src/coco/pause.c
@@ -2,6 +2,8 @@
  * @brief Pause
  */
 
+#ifdef _CMOC_VERSION_
+
 #include <cmoc.h>
 #include <coco.h>
 
@@ -9,3 +11,5 @@ void pause(unsigned char delay)
 {
   sleep(delay/60);
 }
+
+#endif /* _CMOC_VERSION */
