a04ef2d2dcf67c5ea5e7f0798076fa24f1e66627
Tavis Hord <feedback@sideburn.com>
2025-04-03 20:22:27 -0700

    Added required includes, changed ESC key check to 'x' to bypass need for key_codes

--- src/connect_wifi.c
+++ src/connect_wifi.c
@@ -3,7 +3,7 @@
  *
  * Connect to existing WiFi connection
  */
-
+#include <conio.h>
 #include "connect_wifi.h"
 
 #ifdef _CMOC_VERSION_
@@ -82,11 +82,11 @@ void connect_wifi(void)
 
 	while (retries > 0)
 	{
-		// check for ESC key and abort
+		// check for x key and abort
 		if (kbhit()) 
 		{
 			key = cgetc();
-			if (key == KEY_ESCAPE)
+			if (key == 'x')
 			{
 				screen_error("CONNECTION ABORTED");
 				pause(150);
