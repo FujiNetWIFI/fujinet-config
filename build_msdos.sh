91e2e449e877f686fb2b1cf22b0a0290e2734c88
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-08-03 09:28:27 -0500

    add temporary build script.

---
+++ build_msdos.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wcl -q src/main.c src/check_wifi.c src/connect_wifi.c src/destination_host_slot.c src/hosts_and_devices.c src/perform_copy.c src/select_file.c src/select_slot.c src/set_wifi.c src/show_info.c src/msdos/bar.c src/msdos/die.c src/msdos/input.c src/msdos/io.c src/msdos/mount_and_boot.c src/msdos/screen.c
