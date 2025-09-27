42a80aa383ef1a86e02af4c3d7620cbf416405e9
Rich Stephens <richswww@gmail.com>
2025-09-27 19:33:27 +0000

    Update key constants in input.c (#135)

---
+++ src/coco/strendswith.c
@@ -0,0 +1,21 @@
+#ifdef _CMOC_VERSION_
+
+#include <cmoc.h>
+
+/*
+ * strendswith --
+ *
+ * PUBLIC: #ifndef HAVE_STRENDSWITH
+ * PUBLIC: int strendswith __P((const char *, const char *));
+ * PUBLIC: #endif
+ */
+
+int strendswith(const char *str, const char *suffix)
+{
+  size_t str_len = strlen(str);
+  size_t suffix_len = strlen(suffix);
+
+  return (str_len >= suffix_len) &&
+         (!memcmp(str + str_len - suffix_len, suffix, suffix_len));
+}
+#endif /* _CMOC_VERSION_ */
