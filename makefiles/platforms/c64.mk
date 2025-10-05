f455d3b68f39cfb77d18750641606ec8bab02e7e
Chris Osborn <fozztexx@fozztexx.com>
2025-10-05 10:48:48 -0700

    Prepare to link makefiles directly to MekkoGX subtree

--- makefiles/platforms/c64.mk
+++
@@ -1,9 +0,0 @@
-EXECUTABLE = $(R2R_PD)/$(PRODUCT).prg
-DISK = $(R2R_PD)/$(PRODUCT).dsk
-
-MWD := $(realpath $(dir $(lastword $(MAKEFILE_LIST)))..)
-include $(MWD)/common.mk
-include $(MWD)/toolchains/cc65.mk
-
-r2r:: $(EXECUTABLE) $(R2R_EXTRA_DEPS_$(PLATFORM_UC))
-	make -f $(PLATFORM_MK) $(PLATFORM)/r2r-post
