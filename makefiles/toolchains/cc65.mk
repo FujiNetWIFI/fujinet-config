b32217a3b61306f286c5e20de29c6a4baca7d9b8
Chris Osborn <fozztexx@fozztexx.com>
2025-10-14 10:53:31 -0700

    Restore makefiles deleted in rebase

---
+++ makefiles/toolchains/cc65.mk
@@ -0,0 +1,42 @@
+CC_DEFAULT ?= cl65 --color off
+AS_DEFAULT ?= $(CC_DEFAULT)
+LD_DEFAULT ?= $(CC_DEFAULT)
+AR_DEFAULT ?= ar65
+
+include $(MWD)/tc-common.mk
+
+CFLAGS += -O --cpu 6502
+ASFLAGS += --cpu 6502
+LDFLAGS +=
+
+define include-dir-flag
+  --include-dir $1
+endef
+
+define asm-include-dir-flag
+  --asm-include-dir $1
+endef
+
+define library-dir-flag
+  --lib-path $1
+endef
+
+define library-flag
+  $1
+endef
+
+define link-lib
+  $(AR) a $1 $2
+endef
+
+define link-bin
+  $(LD) -vm -t $(PLATFORM) $(LDFLAGS) $2 $(LIBS) -o $1
+endef
+
+define compile
+  $(CC) -l $(basename $1).lst --create-dep $(OBJ_DIR)/$(basename $(notdir $2)).d -c $(CFLAGS) -t $(PLATFORM) -o $1 $2
+endef
+
+define assemble
+  $(AS) -l $(basename $1).lst -c $(ASFLAGS) -t $(PLATFORM) -o $1 $2
+endef
---
+++ makefiles/toolchains/cmoc.mk
@@ -0,0 +1,49 @@
+CC_DEFAULT ?= cmoc
+AS_DEFAULT ?= $(CC_DEFAULT)
+LD_DEFAULT ?= $(CC_DEFAULT)
+AR_DEFAULT = lwar
+
+include $(MWD)/tc-common.mk
+
+CFLAGS += --intdir=$(OBJ_DIR)
+ASFLAGS +=
+LDFLAGS +=
+
+# Needed because of using sed to strip ANSI color escape sequences
+SHELL = /bin/bash -o pipefail
+
+define strip-ansi
+  sed -e 's/'$$'\033''[[][0-9][0-9]*m//g'
+endef
+
+define include-dir-flag
+  -I$1
+endef
+
+define asm-include-dir-flag
+  -I$1
+endef
+
+define library-dir-flag
+  -L$1
+endef
+
+define library-flag
+  -l$1
+endef
+
+define link-lib
+  $(AR) -a -r $@ $^
+endef
+
+define link-bin
+  $(LD) -o $1 $(LDFLAGS) $2 $(LIBS) 2>&1 | $(strip-ansi)
+endef
+
+define compile
+  $(CC) -c $(CFLAGS) --deps=$(OBJ_DIR)/$(basename $(notdir $2)).d -o $1 $2 2>&1 | $(strip-ansi)
+endef
+
+define assemble
+  $(AS) -c $(ASFLAGS) -o $1 $2 2>&1 | $(strip-ansi) | sed -e 's/^\(.*\)(\([0-9][0-9]*\)) :/\1:\2:/'
+endef
