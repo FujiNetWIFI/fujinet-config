accd0206aef4984735b824e69a5dbd650c3bd805
Noah Burney <noah.burney@gmail.com>
2025-10-24 22:31:26 -0500

    msx: vampier font+fanciness wip

--- src/msx/bar.c
+++ src/msx/bar.c
@@ -4,6 +4,7 @@
 
 #include <video/tms99x8.h>
 #include <stdbool.h>
+#include <conio.h>
 
 #define DISPLAY_ROWS 17
 #define DISPLAY_LENGTH 60
@@ -22,7 +23,7 @@
  * static local variables for bar y, max, and index.
  */
 // static unsigned char bar_y=3, bar_c=1, bar_m=1, bar_i=0, bar_oldi=0, bar_co=0x13;
-static unsigned char bar_y=3, bar_c=1, bar_m=1, bar_i=0, bar_oldi=0, bar_co=0x17;
+static unsigned char bar_y=3, bar_c=3, bar_m=1, bar_i=0, bar_oldi=0, bar_co=0x17;
 
 /**
 * Reset text to default
@@ -33,7 +34,19 @@ void bar_clear(bool oldRow)
   // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+(oldRow==true ? bar_oldi : bar_i)), ATTR_PATH_LINE, COL(bar_c));
 
   // Fill background color in old row
-  vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+(oldRow==true ? bar_oldi : bar_i))+COL(bar_c),ATTR_FILE_LIST,256-COL(bar_c));
+  // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+(oldRow==true ? bar_oldi : bar_i))+COL(bar_c),ATTR_FILE_LIST,256-COL(bar_c));
+  //
+  //
+
+  gotoxy(2, bar_y+(oldRow==true ? bar_oldi : bar_i)+1);
+  cputc(' ');
+  gotoxy(30, bar_y+(oldRow==true ? bar_oldi : bar_i)+1);
+  cputc(' ');
+
+  uint16_t addr = ADDR_FILE_LIST + ROW(bar_y+(oldRow==true ? bar_oldi : bar_i))+COL(bar_c)-8;
+  for (uint8_t i = 0; i < 256-COL(bar_c); i++) {
+    vdp_vpoke(addr++, i % 2 ? 0xF4 : 0xF5);
+  }
 }
 
 /**
@@ -45,8 +58,55 @@ void bar_update(void)
 
     bar_clear(true);
 
+    gotoxy(2, bar_y+bar_i+1);
+    // cputc('<');
+    cputc(0x91);
+    gotoxy(30, bar_y+bar_i+1);
+    // cputc('>');
+    cputc(0x92);
+
     // Fill bar color in new row
-    vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff,bar_co,256 - coff);
+    // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff,bar_co,256 - coff);
+    // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff,0x4F,256 - coff);
+    // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff,0x1F,256 - coff);
+
+    // white
+    // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff, 0x1F, 256 - coff - 16);
+    // cyan
+    // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff, 0x17, 256 - coff - 16);
+    // fuschia
+    // vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff, 0xFD, 256 - coff - 16);
+    // red
+    vdp_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff, 0xF6, 256 - coff - 16);
+
+    uint16_t addr = ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff - 8;
+    for (uint8_t i = 0; i < 8; i++) {
+      // white
+      // vdp_vpoke(addr, i % 2 ? 0x4F : 0x5F);
+      // vdp_vpoke(addr + 28*8, i % 2 ? 0x4F : 0x5F);
+      // cyan
+      // vdp_vpoke(addr, i % 2 ? 0x47 : 0x57);
+      // vdp_vpoke(addr + 28*8, i % 2 ? 0x47 : 0x57);
+      // fuschia
+      // vdp_vpoke(addr, i % 2 ? 0x4D : 0x5D);
+      // vdp_vpoke(addr + 28*8, i % 2 ? 0x4D : 0x5D);
+      // red
+      // vdp_vpoke(addr, i % 2 ? 0x46 : 0x56);
+      // vdp_vpoke(addr + 28*8, i % 2 ? 0x46 : 0x56);
+      // red - inverted
+      vdp_vpoke(addr, i % 2 ? 0x64 : 0x65);
+      vdp_vpoke(addr + 28*8, i % 2 ? 0x64 : 0x65);
+
+
+      addr++;
+    }
+
+    // uint16_t addr = ADDR_FILE_LIST + ROW(bar_y+bar_i) + coff;
+    // for (uint8_t i = 0; i < 256-coff; i++) {
+    //   // vdp_vpoke(addr++, i % 2 ? 0xF5 : 0xF7);
+    //   // vdp_vpoke(addr++, i % 2 ? 0x15 : 0x17);
+    //   vdp_vpoke(addr++, i % 2 ? 0x4F : 0x5F);
+    // }
 }
 
 /**
--- src/msx/font.asm
+++ src/msx/font.asm
@@ -2,4 +2,4 @@ SECTION rodata_user
 PUBLIC _font_shifted
 
 _font_shifted:
-    INCBIN "font.bin"
+    INCBIN "vamp_font_3.bin"
