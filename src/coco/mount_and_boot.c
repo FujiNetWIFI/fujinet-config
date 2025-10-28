1c629d24fd8f4c981d75068c24359cc9aa1e14ce
Rich Stephens <richswww@gmail.com>
2025-10-28 09:48:17 -0400

    CoCo: Implement lobby command

--- src/coco/mount_and_boot.c
+++ src/coco/mount_and_boot.c
@@ -1,10 +1,28 @@
 #ifdef _CMOC_VERSION_
 
 #include "mount_and_boot.h"
+#include "../screen.h"
 #include "../typedefs.h"
+#include "../globals.h"
 
 void mount_and_boot_lobby(void)
 {
+	if (screen_mount_and_boot_lobby())
+	{
+		fuji_set_boot_mode(2);
+		system_boot();
+	}
+	else
+	{
+    if (hd_subState == HD_HOSTS)
+    {
+      screen_hosts_and_devices_hosts();
+    }
+    else if (hd_subState == HD_DEVICES)
+    {
+      screen_hosts_and_devices_devices();
+    }
+	}
 }
 
 void mount_and_boot(void)
