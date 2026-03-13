3eb33fed3a1de4e3f9df17110407ae641dd352be
Rich Stephens <richswww@gmail.com>
2026-03-13 14:52:18 -0400

    Update makefiles to match msdos-v2

--- makefiles/platforms/h89-cpm.mk
+++ makefiles/platforms/h89-cpm.mk
@@ -1,14 +1,13 @@
-EXECUTABLE = $(R2R_PD)/$(PRODUCT_BASE).prg
+EXECUTABLE = $(R2R_PD)/$(PRODUCT_BASE).com
 LIBRARY = $(R2R_PD)/$(PRODUCT_BASE).$(PLATFORM).lib
 
 MWD := $(realpath $(dir $(lastword $(MAKEFILE_LIST)))..)
 include $(MWD)/common.mk
 include $(MWD)/toolchains/z88dk.mk
 
-ADAM_FLAGS = +coleco -subtype=adam
-CFLAGS += $(ADAM_FLAGS)
-LDFLAGS += $(ADAM_FLAGS)
-LIBS += -leos.lib -lsmartkeys.lib
+MSX_FLAGS = +cpm -create-app
+CFLAGS += $(MSX_FLAGS)
+LDFLAGS += $(MSX_FLAGS)
 
 r2r:: $(BUILD_EXEC) $(BUILD_LIB) $(R2R_EXTRA_DEPS)
 	make -f $(PLATFORM_MK) $(PLATFORM)/r2r-post
