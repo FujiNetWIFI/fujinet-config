47050d36145cdc9c128ff56a3c656180024dbc3d
Rich Stephens <richswww@gmail.com>
2026-06-07 13:00:39 -0400

    Autoboot ROM files when loaded in drive 0 on coco

--- src/coco/system.c
+++ src/coco/system.c
@@ -1,12 +1,45 @@
 #include "../system.h"
 #include "../pause.h"
+#include "../globals.h"
+#include "strendswith.h"
+
+extern char *screen_upper(char *s);
+
+#define FUJI_IO_REG ((unsigned char *)0xFF41)
 
 static NewDisk newDisk;
 char response[256];
 
+void system_enable_user_rom(void)
+{
+  *FUJI_IO_REG = 5;
+}
+
+bool system_slot0_is_rom(void)
+{
+  static char fn[256];
+  char *name;
+
+  if (!fuji_get_device_filename(0, fn))
+    return false;
+  name = screen_upper(fn);
+  return strendswith(name, ".CCC") || strendswith(name, ".ROM");
+}
+
 void system_boot(void)
 {
-  pause(180); // Pause for 3 seconds to let the user see the mounted disks.
+  if (system_slot0_is_rom()) {
+    printf("\nBOOTING USER ROM...\n");
+    pause(120);                 
+    system_enable_user_rom();   // write 5 to the IO_CONTROL port. Firmware arms the CART trigger and triggers RESET.
+  }
+  else
+  {
+    pause(120);
+  }
+
+  // If the ROM detection code runs on a Rev0000 FujiNet, 
+  // The ROM boot won't happen, so just do a cold start to boot the system normally.
   coldStart();
   exit(0);
 }
